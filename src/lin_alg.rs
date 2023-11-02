af0e78c234473e3c8bb0da8fc13d3e632607c5a0
Layla <chen_rui_tao0525@163.com>
2023-11-02 16:59:30 +0800

    1

--- src/lin_alg.rs
+++ src/lin_alg.rs
@@ -1,29 +1,24 @@
-#[derive(Clone)]
-pub struct Vector {
-    val: Vec<f64>
+#[derive(Clone, Copy)]
+pub struct Vector<const L: usize> {
+    val: [f64; L]
 }
 
-impl Vector {
-    pub fn new(len: usize) -> Self {
-        let mut buf = Vec::new();
-        for _ in 0..len {
-            buf.push(0.0)
-        };
+impl<const L: usize> Vector<L> {
+    pub fn new() -> Self {
         Self {
-            val: buf
+            val: [0.0; L]
         }
+    }
+
+    pub fn from(val: [f64; L]) -> Self {
+        Self { val }
     }
 
     pub fn shape(&self) -> usize {
         self.val.len()
     }
 
-    pub fn eqal_shape(&self, other: &Vector) -> bool {
-        self.shape() == other.shape()
-    }
-
-    pub fn oper_with(&self, rhs: Vector, op: &(dyn Fn(f64, f64) -> f64)) -> Vector {
-        if ! self.eqal_shape(&rhs) {panic!("They don't have the same shape!")}
+    pub fn oper_with(&self, rhs: Vector<L>, op: &(dyn Fn(f64, f64) -> f64)) -> Vector<L> {
         let mut out = self.clone();
         for i in 0..out.shape() {
             out.val[i] = op(out.val[i], rhs.val[i])
@@ -31,48 +26,121 @@ impl Vector {
         out
     }
 
-    pub fn oper(&self, op: &(dyn Fn(f64, f64) -> f64)) -> Vector {
-        if ! self.eqal_shape(&rhs) {panic!("They don't have the same shape!")}
+    pub fn oper(&self, op: &(dyn Fn(f64) -> f64)) -> Vector<L> {
         let mut out = self.clone();
         for i in 0..out.shape() {
-            out.val[i] = op(out.val[i], rhs.val[i])
+            out.val[i] = op(out.val[i])
         };
         out
     }
 }
 
-impl std::ops::Add for Vector {
+impl<const L: usize> std::ops::Add for Vector<L> {
     type Output = Self;
     fn add(self, rhs: Self) -> Self::Output {
         self.oper_with(rhs, &|i,j| { i + j })
     }
 }
 
-impl std::fmt::Display for Vector {
+impl<const L: usize> std::ops::Sub for Vector<L> {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self::Output {
+        self.oper_with(rhs, &|i,j| { i - j })
+    }
+}
+
+impl<const L: usize> std::ops::Mul for Vector<L> {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self::Output {
+        self.oper_with(rhs, &|i,j| { i * j })
+    }
+}
+
+impl<const L: usize> std::ops::Div for Vector<L> {
+    type Output = Self;
+    fn div(self, rhs: Self) -> Self::Output {
+        self.oper_with(rhs, &|i,j| { i / j })
+    }
+}
+
+impl<const L: usize> std::ops::AddAssign for Vector<L> {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = self.oper_with(rhs, &|i ,j| { i + j })
+    }
+}
+
+impl<const L: usize> std::ops::SubAssign for Vector<L> {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = self.oper_with(rhs, &|i ,j| { i - j })
+    }
+}
+
+impl<const L: usize> std::ops::MulAssign for Vector<L> {
+    fn mul_assign(&mut self, rhs: Self) {
+        *self = self.oper_with(rhs, &|i ,j| { i * j })
+    }
+}
+
+impl<const L: usize> std::ops::DivAssign for Vector<L> {
+    fn div_assign(&mut self, rhs: Self) {
+        *self = self.oper_with(rhs, &|i ,j| { i / j })
+    }
+}
+
+impl<const L: usize> std::fmt::Display for Vector<L> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Vector<{}>", self.val.iter().map(|a| {format!("{}", a)}).collect::<Vec<_>>().join(", "))
     }
 }
 
-// impl std::ops::
-
-pub struct Matrix {
-    vec: Vec<Vector>
+#[derive(Clone, Copy)]
+pub struct Matrix<const X: usize, const Y: usize> {
+    vec: [Vector<X>; Y],
 }
 
-impl Matrix {
-    pub fn new(shape:(usize, usize)) -> Self {
-        let mut buf = Vec::new();
-        for _ in 0..shape.0 {
-            buf.push(Vector::new(shape.1))
-        };
+impl<const X: usize, const Y: usize> Matrix<X, Y> {
+    pub fn new() -> Self {
         Self {
-            vec: buf
+            vec: [Vector::new(); Y]
+        }
+    }
+
+    pub fn from(val: [[f64; X]; Y]) -> Self {
+        let mut buf = [Vector::<X>::new(); Y];
+        for i in 0..Y {
+            buf[i] = Vector::from(val[i])
+        }
+        Self { vec: buf }
+    }
+
+    pub fn shape(&self) -> (usize, usize) {
+        (X, Y)
+    }
+
+    pub fn oper(&self, op: &(dyn Fn(usize) -> usize)) -> Self {
+        let mut out = self.clone();
+        for i in out.vec.iter_mut() {
+            
+        }
+        out
+    }
+
+    pub fn change_place(&mut self, x: usize, y: usize, val: f64) {
+        self.vec[y].val[x] = val
+    }
+
+    pub fn change_row(&mut self, y: usize, val: [f64; X]) {
+        self.vec[y] = Vector { val: val };
+    }
+
+    pub fn change_col(&mut self, x: usize, val: [f64; Y]) {
+        for i in 0..self.vec.len() {
+            self.vec[i].val[x] = val[i]
         }
     }
 }
 
-impl std::fmt::Display for Matrix {
+impl<const X: usize, const Y: usize> std::fmt::Display for Matrix<X,Y> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Matrix<\n    {}\n>", self.vec.iter().map(|a| {format!("{}", a)}).collect::<Vec<_>>().join("\n    "))
     }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod lin_alg;
 
 fn main() {
-    let a = lin_alg::Matrix::new((10,10));
+    let a = lin_alg::Matrix::from([
+        [1.0, 1.0], 
+        [1.0, 1.0]
+    ]);
     println!("{}", a);
 }
